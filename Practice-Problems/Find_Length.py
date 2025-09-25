02212aac7e87405e6de13ae059015cd67d4ac4b9
SAKIB-BIN-EHSAN <sakibehsan13@gmail.com>
2025-09-25 21:30:15 +0600

    Submitted 2 practice problems. One for finding the length of a variable and another for finding the type of a variable

---
+++ Practice-Problems/Find_Length.py
@@ -0,0 +1,6 @@
+# Problem: Write a program to find the length of the variable name
+
+variable_name = "Hello World"
+variable_length = len(variable_name)
+
+print(variable_length)
---
+++ Practice-Problems/Find_Type.py
@@ -0,0 +1,6 @@
+# Problem: Write a program to find the type of the variable name
+
+variable_name = "Hello World"
+variable_type = type(variable_name)
+
+print(variable_type)

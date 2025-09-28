324cdf653a89784517efea5523018686d44312db
SAKIB-BIN-EHSAN <sakibehsan13@gmail.com>
2025-09-28 08:57:25 +0600

    Added a python file containing the solutin for finding the index of an item from a list

---
+++ Practice-Problems/Find_Index_From_List.py
@@ -0,0 +1,8 @@
+# Problem: Write a program to find the index of 7
+# numbers = [3, 5, 1, 9, 7, 2, 8 ]
+
+numbers = [3, 5, 1, 9, 7, 2, 8]
+
+# Solution using built-in function
+found_index = numbers.index(7)
+print(found_index)

05bd98b89e00119162845c8ae34119244191a902
SAKIB-BIN-EHSAN <sakibehsan13@gmail.com>
2025-09-30 20:30:40 +0600

    Added a python script which sorts the given numbers in ascending order

---
+++ Practice-Problems/Sort.py
@@ -0,0 +1,11 @@
+# Problem: Write a program to sort the numbers in Ascending order
+
+numbers = [3, 5, 1, 9, 7, 2, 8]
+
+# In ascending order
+print("In ascending order --")
+print(sorted(numbers))
+
+# In descending order
+print("In descending order --")
+print(sorted(numbers, reverse=True))

5a5c4efff5bc0330a76fdd4e55d07a75e5efc0fa
SAKIB-BIN-EHSAN <sakibehsan13@gmail.com>
2025-10-03 20:33:08 +0600

    Added a python script to find an element in the list

---
+++ Practice-Problems/Find_An_Element_In_List.py
@@ -0,0 +1,9 @@
+# Problem: Find if 6 is available in the list
+# my_list = [4, 8, 7, 4, 3, 6, 2, 1, 9]
+
+my_list = [4, 8, 7, 4, 3, 6, 2, 1, 9]
+
+if (6 in my_list):
+    print("6 is available in the list")
+else:
+    print("6 is not available in the list")

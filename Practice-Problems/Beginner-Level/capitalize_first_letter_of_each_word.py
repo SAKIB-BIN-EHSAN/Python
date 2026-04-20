dcba9b4f3fee7689f9b5d0ead12abdafb2d3c3cb
SAKIB-BIN-EHSAN <sakibehsan13@gmail.com>
2026-04-20 21:06:32 +0600

    Added a python script to capitalize each word's first character in a sentence

---
+++ Practice-Problems/Beginner-Level/capitalize_first_letter_of_each_word.py
@@ -0,0 +1,30 @@
+# Problem: Capitalize First Letter of Each Word
+# Build a custom title formatter that capitalizes the first letter of each word without using .title().
+
+# Input: "python for web developers"
+# Output: "Python For Web Developers"
+
+
+def title_formatter(sentence):
+
+    if not isinstance(sentence, str):
+        return "Input must be a string"
+
+    words = sentence.split()
+
+    # convert each word's first character into capitalize form
+    # Note: In Python, parentheses () around a loop like this create a Generator. A generator is like a "lazy" list. It doesn't actually create the list of words in memory immediately; it just prepares the logic to create them when needed.
+    output_sentence = (word[0].upper() + word[1:] for word in words)
+
+    return " ".join(output_sentence)
+
+    # line 14 & line 15 can be written in 1 one like below
+    # return " ".join(word[0].upper() + word[1:] for word in words)
+
+input_sentence = "python for web developers"
+# input_sentence = "this is sakib bin ehsan!"
+# input_sentence = ""
+# input_sentence = 1
+
+print("Input -> ", input_sentence)
+print("Output -> ", title_formatter(input_sentence))
